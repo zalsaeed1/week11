86ed49f6acacf0b4b6f40643106c15f96fdd9d9f
zalsaeed1 <90475126+zalsaeed1@users.noreply.github.com>
2021-11-15 14:15:55 -0600

    Add files via upload

---
+++ Week11.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$txt = "Zahraa Al Saeed";
+$x = 2000;
+$y;
+echo "My name is ";
+echo $txt;
+echo "<br>";
+echo "I am ";
+$y = 2021 - $x;
+echo $y;
+echo "  years old.";
+
+?>
+
+</body>
+</html>
